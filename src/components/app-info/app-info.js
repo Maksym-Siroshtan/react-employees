13bd204ded8c614571a0c1af4c8c966e33785abb
Maksym Siroshtan <106271805+Maksym-Siroshtan@users.noreply.github.com>
2023-06-19 12:15:11 +0300

    Modified employees-list and employees-list-item styles

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -8,7 +8,7 @@ const AppInfo = ({employees, increased}) => {
         <div className="app-info">
             <h1>Учет сотрудников в компании N</h1>
             <h2>Общее число сотрудников: {employees}</h2>
-            <h2>Премию получать: {increased}</h2>
+            <h2>Премию получат: {increased}</h2>
         </div>
     )
 }
